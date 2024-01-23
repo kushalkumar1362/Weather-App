7be3a2d31167f8fe0ba6c01479174b6a3cb5e0bf
kushalkumar1362 <kushalkumar1362@gmail.com>
2024-01-24 00:17:21 +0530

    Add style and functioning on switched tabs

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userInfoContainer = document.querySelector(".user-info-container");
+const grantAccessContainer = document.querySelector(
+  ".grant-location-container"
+);
+const searchForm = document.querySelector("[data-searchForm ]");
+const searchInp = document.querySelector("[data-searchInput]");
+const apiErrorContainer = document.querySelector(".api-error-container");
+
+let currentOpenTab = userTab;
 const API_KEY = "13dc7d33b18a877d68b70594fd9ddad3";
+
+// Setting default tab
+currentOpenTab.classList.add("current-tab");
+
+function switchTab(clickedTab) {
+  apiErrorContainer.classList.remove("active");
+  if (clickedTab != currentOpenTab) {
+    currentOpenTab.classList.remove("current-tab");
+    currentOpenTab = clickedTab;
+    currentOpenTab.classList.add("current-tab");
+
+    if (!searchForm.classList.contains("active")) {
+      userInfoContainer.classList.remove("active");
+      grantAccessContainer.classList.remove("active");
+      searchForm.classList.add("active");
+    } else {
+      searchForm.classList.remove("active");
+      userInfoContainer.classList.remove("active");
+      getFromSessionStorage();
+    }
+  }
+}
+
+function getFromSessionStorage() {
+  const localCoordinates = sessionStorage.getItem("user-coordinates");
+  if (!localCoordinates) {
+    grantAccessContainer.classList.add("active");
+  } else {
+    const coordinates = JSON.parse(localCoordinates);
+  }
+}
+
+userTab.addEventListener("click", () => {
+  switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+  switchTab(searchTab);
+});
